de382f7dea84e5677a68c0933cf20d0293d7cdc6
JohnMHoyt24 <67488201+JohnMHoyt24@users.noreply.github.com>
2025-10-21 14:34:05 +0000

    Added event listeners that change the state when a user clicks on a button.

--- index.js
+++ index.js
@@ -1,3 +1,34 @@
-function submitRating() {
-  
-}
+document.addEventListener('DOMContentLoaded', () => {
+  const ratingForm = document.querySelector('.rating-form');
+  const ratingState = document.getElementById('rating-state');
+  const thankYouState = document.getElementById('thank-you-state');
+  const selectedRatingSpan = document.getElementById('selected-rating');
+  const backButton = document.getElementById('back-button');
+
+  ratingForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const selectedRating = document.querySelector('input[name="rating"]:checked');
+
+    if(selectedRating) {
+      const ratingValue = selectedRating.value;
+
+      selectedRatingSpan.textContent = ratingValue;
+
+      ratingState.classList.add('hidden');
+      thankYouState.classList.remove('hidden');
+    } else{
+      alert('Please select a rating before submitting.');
+    }
+  });
+
+  function resetForm(){
+    ratingState.classList.remove('hidden');
+    thankYouState.classList.add('hidden');
+
+    ratingForm.reset();
+  }
+
+  backButton.addEventListener('click', () => {
+    resetForm();
+  });
+});
